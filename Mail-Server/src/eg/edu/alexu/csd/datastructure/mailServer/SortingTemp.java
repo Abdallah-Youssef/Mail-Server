054dd47bfb776b2645f8ebdab2d148d731e038da
ABE-Mark45 <ahmedinmasr@gmail.com>
2020-04-22 04:55:24 +0200

    Added sorting logic in sorting temp

--- Mail-Server/src/eg/edu/alexu/csd/datastructure/mailServer/SortingTemp.java
+++ Mail-Server/src/eg/edu/alexu/csd/datastructure/mailServer/SortingTemp.java
@@ -1,14 +1,22 @@
 package eg.edu.alexu.csd.datastructure.mailServer;
 
 
+import java.util.Date;
+
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import eg.edu.alexu.csd.datastructure.stack.Classes.Stack;
 
-public class Sorting {
+public class SortingTemp {
 	
-	public static void quickSort(Object[] arr, int l, int h, Comparator comp) {
-		Stack stack = new Stack();
+	
+	public static void quickSort(JSONObject[] arr, ISort comp)
+	{
+		_quickSort(arr, 0, arr.length, comp);
+	}
+	
+	public static void _quickSort(JSONObject[] arr, int l, int h, ISort comp) {
+		StackDS stack = new StackDS();
 		stack.push(l);
 		stack.push(h);
 		
@@ -30,15 +38,15 @@ public class Sorting {
 	}
 	
 	
-	static int partition(Object[] arr, int l, int h, Comparator comp) 
+	static int partition(JSONObject[] arr, int l, int h, ISort comp) 
 	{ 
-		Object x = arr[h]; 
+		JSONObject x = arr[h]; 
 	    int i = (l - 1); 
 	  
 	    for (int j = l; j <= h - 1; j++) { 
 	    	if (comp.compare(arr[j], x) <= 0) {
 	    		i++;
-	    		Object temp = arr[i];
+	    		JSONObject temp = arr[i];
 	    		arr[i] = arr[j];
 	    		arr[j] = temp;
 	    	}
@@ -46,8 +54,65 @@ public class Sorting {
 	    
 	    Object temp = arr[i+1];
 	    arr[i+1] = arr[h];
-	    arr[h] = temp;
+	    arr[h] = (JSONObject) temp;
 	    return (i + 1); 
 	} 
 	
+	
+}
+
+class sortStrings implements ISort
+{
+	String key;
+	
+	public sortStrings(String key) {
+		this.key = key;
+	}
+	
+	public int compare(JSONObject a, JSONObject b)
+	{
+		return ((String)a.get(key)).compareTo((String)b.get(key));
+	}
+}
+
+class sortUserByEmail implements ISort
+{
+	public int compare(JSONObject a, JSONObject b)
+	{
+		String emailA = (String)((JSONArray)a.get("emails")).get(0);
+		String emailB = (String)((JSONArray)a.get("emails")).get(0);
+		return emailA.compareTo(emailB);
+	}
+}
+
+
+class sortUserByDate implements ISort
+{
+	String key;
+	
+	public sortUserByDate(String key) {
+		this.key = key;
+	}
+	
+	public int compare(JSONObject a, JSONObject b)
+	{
+		Date dateA = (Date)(a.get(key));
+		Date dateB = (Date)(a.get(key));
+		return dateA.compareTo(dateB);
+	}
+}
+
+
+class sortNumbers implements ISort
+{
+	String key;
+	
+	public sortNumbers(String key) {
+		this.key = key;
+	}
+
+	public int compare(JSONObject a, JSONObject b)
+	{
+		return (int)a.get(key) - (int)b.get(key);
+	}
 }
