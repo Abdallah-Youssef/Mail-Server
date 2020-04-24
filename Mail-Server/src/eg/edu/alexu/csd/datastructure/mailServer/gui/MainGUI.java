faea34fcba6f163a42069e40a66b23221218a8dd
Abdallah <abdallah.yossf@gmail.com>
2020-04-24 14:54:41 +0200

    ComposeGUI Progress

--- Mail-Server/src/eg/edu/alexu/csd/datastructure/mailServer/gui/MainGUI.java
+++ Mail-Server/src/eg/edu/alexu/csd/datastructure/mailServer/gui/MainGUI.java
@@ -1,14 +1,16 @@
 package eg.edu.alexu.csd.datastructure.mailServer.gui;
 
 import eg.edu.alexu.csd.datastructure.mailServer.FolderManager;
+import eg.edu.alexu.csd.datastructure.mailServer.User;
 
 
 
 public class MainGUI {
 	public static void main(String[] args) {
 		new FolderManager();
+		ComposeGUI.run(new User("test", "test", "test", "test"));
 		FolderManager.printUsers();
-		LoginGUI.run();
+		//LoginGUI.run();
 	}
 }
  
