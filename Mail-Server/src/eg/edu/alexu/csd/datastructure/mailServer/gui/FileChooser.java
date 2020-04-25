311832c501fdb86d40d38bc254b627d5051c7474
youssefhesham2000 <youssefhesham2000@yahoo.com>
2020-04-25 08:36:42 +0200

    finish file chooser

---
+++ Mail-Server/src/eg/edu/alexu/csd/datastructure/mailServer/gui/FileChooser.java
@@ -0,0 +1,88 @@
+package eg.edu.alexu.csd.datastructure.mailServer.gui;
+
+import java.awt.BorderLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.SwingUtilities;
+
+
+public class FileChooser extends JPanel implements ActionListener {
+	static private final String newline = "\n";
+    public String choosedbath;
+	JButton openButton;
+    JTextArea log;
+    JFileChooser fc;
+    public FileChooser() {
+    	super(new BorderLayout());
+    	//create log
+    	log=new JTextArea(5,20);
+    	log.setMargin(new Insets(5,5,5,5));
+    	
+    	log.setEditable(false);
+    	JScrollPane logScrollPane=new JScrollPane(log);
+    	//create a file chooser
+    	fc=new JFileChooser();
+    	openButton=new JButton("Open a file");
+    	openButton.addActionListener(this);
+    	 
+    	JPanel buttonPanel=new JPanel();
+    	buttonPanel.add(openButton);
+    	
+    	add(buttonPanel,BorderLayout.PAGE_START);
+    	add(logScrollPane,BorderLayout.CENTER);
+    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource()==openButton) {
+			int returnVal=fc.showOpenDialog(FileChooser.this);
+			
+			if(returnVal==JFileChooser.APPROVE_OPTION)
+			{
+				File file = fc.getSelectedFile();
+				choosedbath=file.getAbsolutePath();
+				/*
+				 * [think how to return it 
+				 */
+			}
+			else {
+				log.append("Open command cancelled by user." + newline);
+			} 
+		}
+	}
+    
+private static void createAndShowGUI() {
+    //Create and set up the window.
+    JFrame frame = new JFrame("FileChooserDemo");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    //Add content to the window.
+    frame.add(new FileChooser());
+
+    //Display the window.
+    frame.pack();
+    frame.setVisible(true);
+}
+
+public static void main(String[] args) {
+    //Schedule a job for the event dispatch thread:
+    //creating and showing this application's GUI.
+    SwingUtilities.invokeLater(new Runnable() {
+        public void run() {
+            //Turn off metal's use of bold fonts
+           // UIManager.put("swing.boldMetal", Boolean.FALSE); 
+            createAndShowGUI();
+        }
+    });
+}
+}
