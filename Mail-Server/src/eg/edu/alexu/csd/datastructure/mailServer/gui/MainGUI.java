e2c93390e8f07f924d4e5ebb440a9c36a6f85e4f
Abdallah <abdallah.yossf@gmail.com>
2020-05-09 20:43:44 +0200

    hahaha

--- Mail-Server/src/eg/edu/alexu/csd/datastructure/mailServer/gui/MainGUI.java
+++ Mail-Server/src/eg/edu/alexu/csd/datastructure/mailServer/gui/MainGUI.java
@@ -8,9 +8,10 @@ import eg.edu.alexu.csd.datastructure.mailServer.User;
 
 public class MainGUI {
 	public static void main(String[] args) throws IOException {
-		/*FolderManagerBIN.clearUsers();
+		
+		FolderManagerBIN.clearUsers();
 		User test = new User("haha", "popo", "koskos@zobzob.com", "passssap");
-		FolderManagerBIN.addNewUser(test);*/
+		FolderManagerBIN.addNewUser(test);
 
 		User test2 = new User("haha", "popo", "koskos@eltany.com", "passssap");
 		FolderManagerBIN.addNewUser(test2);
